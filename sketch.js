220368a7f92d695fa93136185f4043f186b586e3
craftion <dnzyanik@gmail.com>
2018-11-06 18:33:30 +0100

    new

--- sketch.js
+++ sketch.js
@@ -1,28 +1,60 @@
+var need;
+var act;
+var act1;
 
-var need
-var bal
-function preload(){
+function preload() {
   // put preload code here
-  need = loadImage('./assets/needle.png');
-  bal = loadImage('./assets/balloon.png');
+  need = loadImage('./assets/stage.jpg');
+  act = loadImage('./assets/actor.png');
+  act1 = loadImage('./assets/actor2.png');
+  crowd = loadImage('./assets/crowd.png');;
 }
 
 var mic;
-
-function setup(){
-  createCanvas(windowWidth,windowHeight);
+// var balls;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   mic = new p5.AudioIn()
   mic.start();
-
+  // balls = new ball();
+  // push.balls;
 }
-function draw(){
+
+function draw() {
   background(0);
   micLevel = mic.getLevel();
-  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
+   this.x = width
+   this.y = height
+  image(need, 0,0, windowWidth, windowHeight);
+  image(act1,width/6*2, height/4, act1.width,act1.height);
+  image(act, width/6*2, height/4, act.width,act.height);
+ image(crowd , 0, constrain(height-micLevel*height*3, 0, height), windowWidth, crowd.height*1.2);
 
-  image(need, width/6, constrain(height-micLevel*height*5, 0, height),need.width, need.height);
-  push();
-  
-  image(bal, frameCount, height/2 ,40, 100);
-  pop();
+
+  // if( micLevel > 0,1){
+  //   image(act, width/6*2, height/4, act.width,act.height);
+  // } else{
+  //   image(act1,width/6*2, height/4, act1.width,act1.height);
+  // }
+
+
+
+  // for (var i = 0; i < balls.length; i++) {
+  //   balls[i].show();
+    // balls[i].update();
+  // }
 }
+
+// function ball() {
+//   this.x = width / 2;
+//   this.y = height / 2;
+//   this.speed = 1;
+//
+//   this.show = function() {
+//     image(bal, this.x, this.y, 40, 100);
+//   }
+//   // this.update = function(){
+//   //   this.x -= this.speed;
+//   // }
+//
+// }
